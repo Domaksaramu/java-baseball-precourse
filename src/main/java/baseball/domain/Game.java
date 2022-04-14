30fdd57a649296ec277e2c8335fb738a444ef856
Domaksaramu <jewon945@gmail.com>
2022-04-15 01:02:45 +0900

    feat: 게임 도메인 생성

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,2 +1,49 @@
-package baseball.domain;public class Game {
+package baseball.domain;
+
+public class Game {
+    private Integer strike = 0;
+    private Integer ball = 0 ;
+    private Boolean nothing = false;
+
+    public Integer getBall() {
+        return ball;
+    }
+
+    public Integer getStrike() {
+        return strike;
+    }
+
+    public Boolean getNothing() {
+        return nothing;
+    }
+
+    public void setStrike(Integer strike) {
+        this.strike = strike;
+    }
+
+    public void setBall(Integer ball) {
+        this.ball = ball;
+    }
+
+    public void setNothing(Boolean nothing) {
+        this.nothing = nothing;
+    }
+
+    public void compare(BaseballGameNumber thisNumbers, BaseballGameNumber thatNumbers){
+        for (Integer number: thatNumbers.getNumbers().keySet()) {
+            if(thisNumbers.getNumbers().containsKey(number)){
+                if(thisNumbers.getNumbers().get(number) == thatNumbers.getNumbers().get(number)){
+                    strike+=1;
+                }
+                else{
+                    ball+=1;
+                }
+            }
+            else{
+                continue;
+            }
+        }
+        if(strike==0 && ball==0)
+            nothing = true;
+    }
 }
