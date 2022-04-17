4eb720906bd7d4d40e7550d05b3bd285cf390c80
Domaksaramu <jewon945@gmail.com>
2022-04-18 01:39:06 +0900

    feat: 메인 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,23 @@
 package baseball;
 
+import baseball.controller.BaseballGameService;
+import baseball.domain.number.RandomNumbersGenerator;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
+    private static Scanner scanner = new Scanner(System.in);
+    private static RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator();
+    private static BaseballGameService baseballGameService = new BaseballGameService();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        Boolean contineFlag = true;
+        while(contineFlag){
+            List<Integer> randomList = randomNumbersGenerator.getRandomList();
+            baseballGameService.startBaseGame(scanner,randomList);
+            contineFlag = baseballGameService.checkRestartGame(scanner);
+        }
     }
 }
